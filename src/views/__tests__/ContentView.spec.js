1432e6af5f708880cc3ca3098db05b8042e8e23a
Giuseppe DeMarino <giuseppe.demarino@sainsburys.co.uk>
2020-01-28 13:14:38 +0000

    moved fixture folder outside tests folder

--- src/views/__tests__/ContentView.spec.js
+++ src/views/__tests__/ContentView.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { cleanup, render, waitForElement } from '@testing-library/react';
 import { getResponse } from '../../utils';
-import { resultsTravel, resultsUkNews } from './fixtures/data';
+import { resultsTravel, resultsUkNews } from '../fixtures/data';
 
 import '@testing-library/jest-dom/extend-expect';
 import ContentView from '../ContentView';
